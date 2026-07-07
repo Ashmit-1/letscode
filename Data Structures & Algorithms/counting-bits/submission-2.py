1c87547cfff93f8e77818f3a20e800913ab42be9
dev_das <137298461+Ashmit-1@users.noreply.github.com>
2026-07-07 13:59:17 +0530

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        from functools import lru_cache
+
+        # @lru_cache
+        def countBits(n):
+            count = 0
+            while n != 0:
+                if n & 1: count+=1
+                n = n >> 1
+            return count
+        
+        ret = []
+        for i in range(n+1):
+            res = 0
+            res+=countBits(i)
+            ret.append(res)
+        return ret
+        
+        

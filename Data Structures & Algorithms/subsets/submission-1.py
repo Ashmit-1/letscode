1a8702f1fc4f8dc7fce0a56d78735874801fb539
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2026-09-06 15:16:23 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def find_sub(ind, nums, ls, ans):
+            if ind == len(nums):
+                ans.append(ls.copy())
+                return 
+            ls.append(nums[ind])
+            find_sub(ind+1, nums, ls, ans)
+            ls.pop()
+            find_sub(ind+1, nums, ls, ans)
+        ans = []
+        find_sub(0, nums, [], ans)
+        return ans
+        
